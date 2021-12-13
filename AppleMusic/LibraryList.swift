4bf6a2f8ce7b2eeb4316fd2bf459c7ed8ee7a01a
GonyaDev <gonyadev@yandex.ru>
2021-12-13 10:51:48 +0300

    Create iconSelection
    Create iconSelection Property

---
+++ AppleMusic/LibraryList.swift
@@ -0,0 +1,20 @@
+//
+//  LibraryList.swift
+//  AppleMusic
+//
+//  Created by Алексей Гончаров on 13.12.2021.
+//
+
+import SwiftUI
+
+struct LibraryList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LibraryList_Previews: PreviewProvider {
+    static var previews: some View {
+        LibraryList()
+    }
+}
