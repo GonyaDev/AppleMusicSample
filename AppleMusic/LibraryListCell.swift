a44e807f1f7bf88c1dcaefd0e51238ea59104094
GonyaDev <gonyadev@yandex.ru>
2021-12-13 10:56:25 +0300

    Create LibraryViewCell
    Create LibraryViewCell file

---
+++ AppleMusic/LibraryListCell.swift
@@ -0,0 +1,20 @@
+//
+//  LibraryListCell.swift
+//  AppleMusic
+//
+//  Created by Алексей Гончаров on 13.12.2021.
+//
+
+import SwiftUI
+
+struct LibraryListCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LibraryListCell_Previews: PreviewProvider {
+    static var previews: some View {
+        LibraryListCell()
+    }
+}
