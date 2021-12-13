6ff2209041efd2f141ea76c6651fa1dedd8f9775
GonyaDev <gonyadev@yandex.ru>
2021-12-13 11:17:47 +0300

    Create Cells
    Creating cells with icons

--- AppleMusic/LibraryListCell.swift
+++ AppleMusic/LibraryListCell.swift
@@ -8,13 +8,40 @@
 import SwiftUI
 
 struct LibraryListCell: View {
+    
+    var icons: Icons
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            icons.icon
+                .resizable()
+                .scaledToFit()
+                .frame(width: Metrics.listIconSize,
+                       height: Metrics.listIconSize
+                       )
+                .foregroundColor(.pink)
+            
+            Text(icons.iconName)
+                .padding(.leading, Metrics.ListTitlePaddingToLeft)
+                .font(.title2)
+            
+            Spacer()
+            
+            
+        }
+        .frame(height: Metrics.listRowHeight)
     }
 }
 
-struct LibraryListCell_Previews: PreviewProvider {
-    static var previews: some View {
-        LibraryListCell()
+//Create an extension with metrics
+
+extension LibraryListCell {
+    
+    //Metrics
+    
+    enum Metrics {
+        static let listRowHeight: CGFloat = 40
+        static let listIconSize: CGFloat = 25
+        static let ListTitlePaddingToLeft: CGFloat = 6
     }
 }
