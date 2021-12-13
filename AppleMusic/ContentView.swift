7aff0a2f6316e9b39baa8b71b2a416aebcd2b932
GonyaDev <gonyadev@yandex.ru>
2021-12-13 10:26:45 +0300

    Change Color
    The color has been changed to pink

--- AppleMusic/ContentView.swift
+++ AppleMusic/ContentView.swift
@@ -32,6 +32,7 @@ struct ContentView: View {
                     Text("Поиск")
                 }
         }
+        .accentColor(.pink)
     }
 }
 
--- AppleMusic/LibraryView.swift
+++ AppleMusic/LibraryView.swift
@@ -8,24 +8,37 @@
 import SwiftUI
 
 struct LibraryView: View {
+    
+    
     var body: some View {
         
         //Creating navigation view to put the text inside
-        NavigationView {
-            
-            VStack {
-                Text("Ищите свою музыку?")
-                    .bold()
-                    .font(.system(size: 20))
-                    .padding(.bottom, 0.5)
-                Text("Здесь появится купленная вами в \n iTunes Store музыка")
-                    .foregroundColor(.secondary)
-                    .multilineTextAlignment(.center)
-                    .font(.subheadline)
-                    .padding([.leading, .trailing], 15)
+        
+            NavigationView {
+                
+                VStack {
+                    Text("Ищите свою музыку?")
+                        .bold()
+                        .font(.system(size: 20))
+                        .padding(.bottom, 0.5)
+                    Text("Здесь появится купленная вами в \n iTunes Store музыка")
+                        .foregroundColor(.secondary)
+                        .multilineTextAlignment(.center)
+                        .font(.subheadline)
+                        .padding([.leading, .trailing], 15)
+                }
+                .navigationTitle("Медиатека")
+                .toolbar {
+                    
+                    ToolbarItem() {
+                        Button(action: {}, label: {
+                            Text("Править")
+                        })
+                    }
+                }
             }
-            .navigationTitle("Медиатека")
-        }
+            .accentColor(.pink)
+        
     }
 }
 
