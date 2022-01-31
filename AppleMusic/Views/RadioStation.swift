381a0c0c78d268506ff96f927d7e9879533155c5
GonyaDev <gonyadev@yandex.ru>
2022-02-01 00:13:44 +0300

    Completing the list of music (stations)

--- AppleMusic/Views/RadioStation.swift
+++ AppleMusic/Views/RadioStation.swift
@@ -20,14 +20,14 @@ struct RadioStation: Hashable, Identifiable {
 }
 
 var listOfStations: [RadioStation] = [
-    RadioStation(title: "Хиты на русском ", subTitle: "Станция Apple Music", imageName: <#T##String#>),
-    RadioStation(title: "Хип-хоп на русском", subTitle: "Станция Apple Music", imageName: <#T##String#>),
-    RadioStation(title: "Танцевальная музыка", subTitle: "Станция Apple Music", imageName: <#T##String#>),
-    RadioStation(title: "Поп на русском", subTitle: "Станция Apple Music", imageName: <#T##String#>),
-    RadioStation(title: "Инди на русском", subTitle: "Станция Apple Music", imageName: <#T##String#>),
-    RadioStation(title: "Рок", subTitle: "Станция Apple Music", imageName: <#T##String#>),
-    RadioStation(title: "Lo-fi", subTitle: "Станция Apple Music", imageName: <#T##String#>),
-    RadioStation(title: "Хип-хоп и R&B", subTitle: "Станция Apple Music", imageName: <#T##String#>),
-    RadioStation(title: "Инди", subTitle: "Станция Apple Music", imageName: <#T##String#>),
-    RadioStation(title: "Детские песни на русском", subTitle: "Станция Apple Music", imageName: <#T##String#>),
+    RadioStation(title: "Хиты на русском ", subTitle: "Станция Apple Music", imageName: "ru-hits-station"),
+    RadioStation(title: "Хип-хоп на русском", subTitle: "Станция Apple Music", imageName: "ru-hiphop-station"),
+    RadioStation(title: "Христианская музыка", subTitle: "Станция Apple Music", imageName: "christian-station"),
+    RadioStation(title: "Поп на русском", subTitle: "Станция Apple Music", imageName: "rupop-station"),
+    RadioStation(title: "Инди на русском", subTitle: "Станция Apple Music", imageName: "ruindi-station"),
+    RadioStation(title: "Фортепиано", subTitle: "Станция Apple Music", imageName: "piano-station"),
+    RadioStation(title: "Техно", subTitle: "Станция Apple Music", imageName: "techno-station"),
+    RadioStation(title: "Блюз", subTitle: "Станция Apple Music", imageName: "blues-station"),
+    RadioStation(title: "Классика", subTitle: "Станция Apple Music", imageName: "classical-station"),
+    RadioStation(title: "Детские песни на русском", subTitle: "Станция Apple Music", imageName: "disney-music-station"),
 ]
