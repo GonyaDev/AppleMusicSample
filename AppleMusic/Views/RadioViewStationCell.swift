4219c313fc767c2457493154028a893cd317be33
GonyaDev <gonyadev@yandex.ru>
2022-02-01 01:16:00 +0300

    creating cell for stations

---
+++ AppleMusic/Views/RadioViewStationCell.swift
@@ -0,0 +1,20 @@
+//
+//  RadioViewStationCell.swift
+//  AppleMusic
+//
+//  Created by Алексей Гончаров on 01.02.2022.
+//
+
+import SwiftUI
+
+struct RadioViewStationCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RadioViewStationCell_Previews: PreviewProvider {
+    static var previews: some View {
+        RadioViewStationCell()
+    }
+}
