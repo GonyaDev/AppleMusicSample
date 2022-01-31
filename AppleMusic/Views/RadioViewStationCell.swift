848872e257948a67533bf1b9e0db86b3f07ef93f
GonyaDev <gonyadev@yandex.ru>
2022-02-01 01:16:10 +0300

    creating cells

--- AppleMusic/Views/RadioViewStationCell.swift
+++ AppleMusic/Views/RadioViewStationCell.swift
@@ -8,13 +8,38 @@
 import SwiftUI
 
 struct RadioViewStationCell: View {
+    
+    let radioStation: RadioStation
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 9) {
+            HStack {
+                radioStation.image
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(RoundedRectangle(cornerRadius: 6))
+                    .frame(width: 100, height: 100)
+                
+                VStack(alignment: .leading) {
+                    Text(radioStation.title)
+                        .font(.title3)
+                    
+                    if let subTitle = radioStation.subTitle {
+                        Text(subTitle)
+                            .foregroundColor(.secondary)
+                }
+            }
+            .padding(.leading, 7)
+            }
+            Divider()
+                .padding(.leading, 115)
+        }
     }
 }
 
 struct RadioViewStationCell_Previews: PreviewProvider {
     static var previews: some View {
-        RadioViewStationCell()
+        RadioViewStationCell(radioStation: listOfStations[1])
+            .previewLayout(.fixed(width: 400, height: 400))
     }
 }
